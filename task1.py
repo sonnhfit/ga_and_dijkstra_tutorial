87cd6cf978bd398400cf12342d0276025a2f6077
sonnhfit <sonnhfit@gmail.com>
2019-03-11 19:33:47 +0700

    python basic

---
+++ task1.py
@@ -0,0 +1,3 @@
+#vong lap for chay tu 0 - 9
+for i in range(10):
+    print(i)
---
+++ task2.py
@@ -0,0 +1,15 @@
+#cach tao ra mot cai mang
+
+#khai bao mang
+a = []
+
+#them mot phan tu vao cuoi mang
+
+a.append(4)
+a.append(5)
+
+#in ra mang
+print(a)
+
+
+
