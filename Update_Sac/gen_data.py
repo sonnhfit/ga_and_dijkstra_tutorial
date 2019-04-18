14df3a804bc5d7c373c5e270ff9e8e34a8aeb327
sonnhfit <sonnhfit@gmail.com>
2019-04-18 16:18:12 +0700

    yến chó

--- Update_Sac/gen_data.py
+++ Update_Sac/gen_data.py
@@ -1,22 +1,33 @@
+import numpy as np
+import pandas as pd
 import csv
 import random
 from random import randint
 
 array_label = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'k', 'l', 'm', 'n','p', 'v', 'o', 'i', 'u', 'y', 't', 'r', 'e', 'w', 'q', 's', 'x', 'z']
 
+random.seed(9001)
+
 
 def choice_to(array, from_el):
     while True:
-        if random.choice(array_label) != from_el:
-            return from_el
+        rd = random.choice(array)
+        if rd != from_el:
+            return rd
 
 solanlap = 10
+ar = []
+for i in range(solanlap):
+    pre_choice = random.choice(array_label)
+    pre2_choice = choice_to(array_label, pre_choice)
+    rdint = randint(2, 90)
+    sub_ar = [pre_choice, pre2_choice, rdint]
+    ar.append(sub_ar)
 
-with open('data.csv', mode='w') as csv_file:
-    fieldnames = ['from', 'to', 'cost']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+# Creating a 2 dimensional numpy array
+data= np.array(ar)
 
-    writer.writeheader()
-    for i in range(solanlap):
-        pre_choice = random.choice(array_label)
-        writer.writerow({'from': pre_choice, 'to': choice_to(array_label, pre_choice), 'cost': randint(2, 90)})
+#Creating pandas dataframe from numpy array
+dataset = pd.DataFrame({'from':data[:,0],'to':data[:,1], 'cost': data[:, 2]})
+dataset.to_csv('data.csv', index=None, )
+print('oke')
