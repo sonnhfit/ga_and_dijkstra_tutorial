9b00e258429c4d9f41e5d11657f7ff572778e00a
sonnhfit <sonnhfit@gmail.com>
2019-04-22 10:48:53 +0700

    Update yencho.py

--- Update_Sac/yencho.py
+++ Update_Sac/yencho.py
@@ -47,90 +47,90 @@ def make_edge(start, end, cost=1):
 
 f= open("out.txt","w+")
 
-class Graph:
-    def __init__(self, edges):
-        # let's check that the data is right
-        # lay nhung canh ma khong co 2 hoac 3 phan tu nhet vao mang wrong_edges
-        wrong_edges = [i for i in edges if len(i) not in [2, 3]]
-        # neu ma co canh loi thi in canh do ra
-        if wrong_edges:
-            raise ValueError('Wrong edges data: {}'.format(wrong_edges))
-        # dat ten cho tung thuoc tinh cua  edge
-        self.edges = [make_edge(*edge) for edge in edges]
+# class Graph:
+#     def __init__(self, edges):
+#         # let's check that the data is right
+#         # lay nhung canh ma khong co 2 hoac 3 phan tu nhet vao mang wrong_edges
+#         wrong_edges = [i for i in edges if len(i) not in [2, 3]]
+#         # neu ma co canh loi thi in canh do ra
+#         if wrong_edges:
+#             raise ValueError('Wrong edges data: {}'.format(wrong_edges))
+#         # dat ten cho tung thuoc tinh cua  edge
+#         self.edges = [make_edge(*edge) for edge in edges]
 
-    # thuoc tinh
-    @property
-    def vertices(self):
-        return set(
-            sum(
-                ([edge.start, edge.end] for edge in self.edges), []
-            )
-        )
+#     # thuoc tinh
+#     @property
+#     def vertices(self):
+#         return set(
+#             sum(
+#                 ([edge.start, edge.end] for edge in self.edges), []
+#             )
+#         )
 
-    def get_node_pairs(self, n1, n2, both_ends=True):
-        if both_ends:
-            node_pairs = [[n1, n2], [n2, n1]]
-        else:
-            node_pairs = [[n1, n2]]
-        return node_pairs
+#     def get_node_pairs(self, n1, n2, both_ends=True):
+#         if both_ends:
+#             node_pairs = [[n1, n2], [n2, n1]]
+#         else:
+#             node_pairs = [[n1, n2]]
+#         return node_pairs
 
-    def remove_edge(self, n1, n2, both_ends=True):
-        node_pairs = self.get_node_pairs(n1, n2, both_ends)
-        edges = self.edges[:]
-        for edge in edges:
-            if [edge.start, edge.end] in node_pairs:
-                self.edges.remove(edge)
+#     def remove_edge(self, n1, n2, both_ends=True):
+#         node_pairs = self.get_node_pairs(n1, n2, both_ends)
+#         edges = self.edges[:]
+#         for edge in edges:
+#             if [edge.start, edge.end] in node_pairs:
+#                 self.edges.remove(edge)
 
-    def add_edge(self, n1, n2, cost=1, both_ends=True):
-        node_pairs = self.get_node_pairs(n1, n2, both_ends)
-        for edge in self.edges:
-            if [edge.start, edge.end] in node_pairs:
-                return ValueError('Edge {} {} already exists'.format(n1, n2))
+#     def add_edge(self, n1, n2, cost=1, both_ends=True):
+#         node_pairs = self.get_node_pairs(n1, n2, both_ends)
+#         for edge in self.edges:
+#             if [edge.start, edge.end] in node_pairs:
+#                 return ValueError('Edge {} {} already exists'.format(n1, n2))
 
-        self.edges.append(Edge(start=n1, end=n2, cost=cost))
-        if both_ends:
-            self.edges.append(Edge(start=n2, end=n1, cost=cost))
+#         self.edges.append(Edge(start=n1, end=n2, cost=cost))
+#         if both_ends:
+#             self.edges.append(Edge(start=n2, end=n1, cost=cost))
 
-    @property
-    def neighbours(self):
-        neighbours = {vertex: set() for vertex in self.vertices}
-        for edge in self.edges:
-            neighbours[edge.start].add((edge.end, edge.cost))
+#     @property
+#     def neighbours(self):
+#         neighbours = {vertex: set() for vertex in self.vertices}
+#         for edge in self.edges:
+#             neighbours[edge.start].add((edge.end, edge.cost))
 
-        return neighbours
+#         return neighbours
 
-    def dijkstra(self, source, dest):
-        assert source in self.vertices, 'Such source node doesn\'t exist'
-        distances = {vertex: inf for vertex in self.vertices}
-        previous_vertices = {
-            vertex: None for vertex in self.vertices
-        }
-        distances[source] = 0
-        vertices = self.vertices.copy()
+#     def dijkstra(self, source, dest):
+#         assert source in self.vertices, 'Such source node doesn\'t exist'
+#         distances = {vertex: inf for vertex in self.vertices}
+#         previous_vertices = {
+#             vertex: None for vertex in self.vertices
+#         }
+#         distances[source] = 0
+#         vertices = self.vertices.copy()
 
-        while vertices:
-            current_vertex = min(
-                vertices, key=lambda vertex: distances[vertex])
-            vertices.remove(current_vertex)
-            if distances[current_vertex] == inf:
-                break
-            for neighbour, cost in self.neighbours[current_vertex]:
-                alternative_route = distances[current_vertex] + cost
-                if alternative_route < distances[neighbour]:
-                    distances[neighbour] = alternative_route
-                    previous_vertices[neighbour] = current_vertex
+#         while vertices:
+#             current_vertex = min(
+#                 vertices, key=lambda vertex: distances[vertex])
+#             vertices.remove(current_vertex)
+#             if distances[current_vertex] == inf:
+#                 break
+#             for neighbour, cost in self.neighbours[current_vertex]:
+#                 alternative_route = distances[current_vertex] + cost
+#                 if alternative_route < distances[neighbour]:
+#                     distances[neighbour] = alternative_route
+#                     previous_vertices[neighbour] = current_vertex
 
-        path, current_vertex = deque(), dest
-        while previous_vertices[current_vertex] is not None:
-            path.appendleft(current_vertex)
-            current_vertex = previous_vertices[current_vertex]
-        if path:
-            path.appendleft(current_vertex)
-        # print(distances[dest])
-        return path, distances[dest]
+#         path, current_vertex = deque(), dest
+#         while previous_vertices[current_vertex] is not None:
+#             path.appendleft(current_vertex)
+#             current_vertex = previous_vertices[current_vertex]
+#         if path:
+#             path.appendleft(current_vertex)
+#         # print(distances[dest])
+#         return path, distances[dest]
 
 
-graph = Graph(trunggian)
+# graph = (trunggian)
 PIN = 57
 
 # print(graph.dijkstra('c', 'h'))
@@ -154,20 +154,20 @@ def sac_pin(gen):
     path = []
     dis = 0
     is_sac = True
-    for i in range(1, len(gen)):
-        path_temp, dis_temp = graph.dijkstra(pre_step, gen[i])
-        dis = dis + dis_temp
-        try:
-            val = int(gen[i])
-            is_sac = True
-            return False
-        except ValueError:
-            is_sac = False
-        if dis > PIN:
-            return True
-        #path.append(path_temp)
-        #dis = dis + dis_temp
-        pre_step = gen[i]
+    #     for i in range(1, len(gen)):
+    #         #path_temp, dis_temp = graph.dijkstra(pre_step, gen[i])
+    #         dis = dis + dis_temp
+    #         try:
+    #             val = int(gen[i])
+    #             is_sac = True
+    #             return False
+    #         except ValueError:
+    #             is_sac = False
+    #         if dis > PIN:
+    #             return True
+    #         #path.append(path_temp)
+    #         #dis = dis + dis_temp
+    #         pre_step = gen[i]
     return False
 
 
@@ -192,14 +192,14 @@ def add_tram_sac(gen, tram_sac):
     path = []
     dis = 0
     is_sac = True
-    for i in range(1, len(gen)):
-        path_temp, dis_temp = graph.dijkstra(pre_step, gen[i])
-        dis = dis + dis_temp
-        if dis > PIN:
-            gen.insert(i-1,tram_sac.pop())
-            return gen
-        #path.append(path_temp)
-        #dis = dis + dis_temp
+    #     for i in range(1, len(gen)):
+    #         path_temp, dis_temp = graph.dijkstra(pre_step, gen[i])
+    #         dis = dis + dis_temp
+    #         if dis > PIN:
+    #             gen.insert(i-1,tram_sac.pop())
+    #             return gen
+    #         #path.append(path_temp)
+    #         #dis = dis + dis_temp
     return gen
 
 
